2fac21fe4b43ea04821a4ffa04d9813679d30424
ashique357 <ashiquejubayer357@gmail.com>
2020-08-29 04:04:06 -0700

    secondary-1

--- routes/api.php
+++ routes/api.php
@@ -9,8 +9,16 @@ use App\QuizWinner;
 use App\GoldHistory;
 
 Route::get('/admin/daily-quiz/today',function(){
+
     $today=Carbon::today()->isoFormat('Do MMMM YYYY');
     $quiz=Quiz::where(('date'),'=',$today)->get();
     return response()->json($quiz);
 });
 
+// Route::post('/daily-quiz/today',function(Request $request){
+    
+//     $data=$request->all();
+//     // QuizWinner::create($data);
+//     // return response()->json($data);
+//     dd($data);
+// });
